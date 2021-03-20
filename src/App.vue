9e09da47f298bd02a6ebd1df404e139d52b23975
neodigm <3151842+neodigm@users.noreply.github.com>
2021-03-20 13:34:19 -0500

    duck

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,38 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar
+    <v-navigation-drawer
+      v-model="drawer"
       app
-      color="white"
-      flat
     >
-      <v-avatar
-        :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
-        size="32"
-      ></v-avatar>
+      <!--  -->
+    </v-navigation-drawer>
 
-      <v-tabs
-        centered
-        class="ml-n9"
-        color="grey darken-1"
-      >
-        <v-tab
-          v-for="link in links"
-          :key="link.caption"
-          :to="link.route"
-        >
-          {{ link.caption }}
-        </v-tab>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer">
 
-      </v-tabs>
+      </v-app-bar-nav-icon>
 
-      <v-avatar
-        class="hidden-sm-and-down"
-        color="grey darken-1 shrink"
-        size="32"
-      ></v-avatar>
+      <v-toolbar-title>emailreplytime</v-toolbar-title>
+
+<v-tabs
+  centered
+  class="ml-n9"
+  color="grey darken-1"
+>
+  <v-tab
+    v-for="link in links"
+    :key="link.caption"
+    :to="link.route"
+  >
+    {{ link.caption }}
+  </v-tab>
+
+</v-tabs>
+
     </v-app-bar>
 
-    <v-main class="grey lighten-3">
-      <v-container>
-        <v-row>
-          <v-col
-            cols="12"
-            sm="2"
-          >
-            <v-sheet
-              rounded="lg"
-              min-height="268"
-            >
-              <!--  -->
-            </v-sheet>
-          </v-col>
-
-          <v-col
-            cols="12"
-            sm="8"
-          >
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
-            >
-            <router-view></router-view>
-              <!--  -->
-            </v-sheet>
-          </v-col>
-
-          <v-col
-            cols="12"
-            sm="2"
-          >
-            <v-sheet
-              rounded="lg"
-              min-height="268"
-            >
-<!--  -->
-            </v-sheet>
-          </v-col>
-        </v-row>
-
-
-      </v-container>
+    <v-main>
+    <router-view></router-view>
     </v-main>
   </v-app>
 </template>
@@ -82,6 +40,7 @@
 <script>
   export default {
     data: () => ({
+      drawer: null ,
       links: [
         {"caption": "Dashboard", "route": "About"},
         {"caption": "Messages", "route": "About"},
