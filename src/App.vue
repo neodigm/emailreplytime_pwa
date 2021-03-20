af192459a8da188a69ea13c578c55a6763a028b4
neodigm <3151842+neodigm@users.noreply.github.com>
2021-03-20 11:39:20 -0500

    duck

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      color="white"
+      flat
+    >
+      <v-avatar
+        :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
+        size="32"
+      ></v-avatar>
+
+      <v-tabs
+        centered
+        class="ml-n9"
+        color="grey darken-1"
+      >
+        <v-tab
+          v-for="link in links"
+          :key="link.caption"
+          :to="link.route"
+        >
+          {{ link.caption }}
+        </v-tab>
+
+      </v-tabs>
+
+      <v-avatar
+        class="hidden-sm-and-down"
+        color="grey darken-1 shrink"
+        size="32"
+      ></v-avatar>
+    </v-app-bar>
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            sm="2"
+          >
+            <v-sheet
+              rounded="lg"
+              min-height="268"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+
+          <v-col
+            cols="12"
+            sm="8"
+          >
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+            <router-view></router-view>
+              <!--  -->
+            </v-sheet>
+          </v-col>
+
+          <v-col
+            cols="12"
+            sm="2"
+          >
+            <v-sheet
+              rounded="lg"
+              min-height="268"
+            >
+<!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+
+
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      links: [
+        {"caption": "Dashboard", "route": "About"},
+        {"caption": "Messages", "route": "About"},
+        {"caption": "Profile", "route": "About"},
+        {"caption": "Updates", "route": "EnhancedTable"}
+      ],
+    }),
+  }
+</script>
